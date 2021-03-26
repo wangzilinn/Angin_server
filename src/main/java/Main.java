4f568390f1b2f0887caa0dfd0beeb7c96cb3d34d
wangzilin <wangzilinn@gmail.com>
2021-03-26 19:53:49 +0800

    first commit:hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: wangzilinn@gmail.com
+ * @Date: 3/26/2021 7:44 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
